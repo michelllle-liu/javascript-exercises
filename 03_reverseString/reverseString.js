4ee5d240dcdb142094a3fea7b5387304b5c5ed3e
michelllle-liu <michelle.01.liu@outlook.com>
2022-02-09 19:44:58 -0400

    Edit reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stack = string.split("");
+    let i = 0;
+    let reversed = "";
 
+    while (i < stack.length)
+        reversed += stack.pop();
+    
+    return reversed;
 };
 
 // Do not edit below this line
