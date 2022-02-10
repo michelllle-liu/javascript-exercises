c13be256d4cd4ef224e5bd93ddde0aa1020f996b
michelllle-liu <michelle.01.liu@outlook.com>
2022-02-09 20:12:22 -0400

    Edit removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
+    // array.splice(array.indexOf(element), 1);
+    // return array;
 
+    for (let i = 1; i < arguments.length; i++){
+        index = array.indexOf(arguments[i]);
+        
+        if (index != -1)
+            array.splice(index, 1);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
