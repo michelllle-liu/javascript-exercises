161c2c1f85b83fa46d03bf1300d0ef68db9b2664
michelllle-liu <michelle.01.liu@outlook.com>
2022-07-20 18:36:50 -0400

    Add sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    sum = 0;
 
+    if ((!Number.isInteger(start)) || (!Number.isInteger(stop)))
+        return 'ERROR'
+
+    if ((start < 0) || (stop < 0))
+        return 'ERROR'
+
+    if (start > stop){
+        let temp = start;
+        start = stop;
+        stop = temp;
+    }
+
+    for (let i = start; i <= stop; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
